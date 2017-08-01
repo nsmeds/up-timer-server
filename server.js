cfe087fbbdebd22901d7269b741ff4d99d140d49
nsmeds <niilosmeds@gmail.com>
2017-08-01 14:08:01 -0700

    api calls for get and edit

---
+++ server.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const app = express();
+// const http = require('http');
+// const port = process.env.PORT || 3000;
+
+// const server = http.createServer(app);
+
+// server.listen(port, () => {
+//     console.log('server running on', server.address());
+// });
