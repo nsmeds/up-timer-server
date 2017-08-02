35e8238eb2d1ab6d2052bd1b6c7dfe6547ed6085
nsmeds <niilosmeds@gmail.com>
2017-08-01 17:20:50 -0700

    cron runs

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const app = require('./lib/app');
 const http = require('http');
+const cron = require('./lib/cron');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+
+cron();
 
 server.listen(port, () => {
     console.log('server running on', server.address());
---
+++ test/app.test.js
@@ -0,0 +1,21 @@
+const mocha = require('mocha');
+const assert = require('chai').assert;
+const app = require('../lib/app');
+const monitors = require('../monitors');
+
+describe('upTimer api', function() {
+
+    before(() => app.getMonitors());
+    
+    it('gets all monitors', function(done) {
+        app.getMonitors();
+        assert.equal('hi', app.getMonitors());
+        done();
+    });
+
+    it('starts a monitor', function(done) {
+        app.resume(monitors.portamentoId);
+        assert.equal('hi', 'hi');
+        done();
+    })
+});
